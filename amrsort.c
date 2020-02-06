f17ec2316c175651aadbc8a26904a00a9cc72fbc
AmrutheshwarO <55506973+AmrutheshwarO@users.noreply.github.com>
2020-02-06 19:26:40 +0530

    Add files via upload

---
+++ amrsort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void main(){
+	int a[1000000], n, i, j, temp, r;
+	clock_t start, end;
+	double tt;
+	printf("Enter the number of elements.\n");
+	scanf("%d", &n);
+	for(i=0; i<n;  i++){
+		r=rand();
+		a[i]=r;
+	}
+	start=clock();
+	for(i=0; i<n; i++)
+		for(j=0; j<i; j++)
+			if(a[j]>a[i]){
+				temp=a[j];
+				a[j]=a[i];
+				a[i]=temp;
+			}
+	end=clock();
+	tt=(double)(end-start)/CLOCKS_PER_SEC;
+	printf("Time taken= %lf\n", tt);
+}
